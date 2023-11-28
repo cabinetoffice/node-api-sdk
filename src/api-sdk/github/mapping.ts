da26a1ed055b625e44ee444976bb914705580811
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-11-28 17:03:18 +0000

    update mapping so it correctly loops through the github api results

--- src/api-sdk/github/mapping.ts
+++ src/api-sdk/github/mapping.ts
@@ -1,33 +1,33 @@
 import { GitHubMembers, GitHubRepos, GitHubTeams } from './type';
 
-export const reposMapping = (body: any): GitHubRepos => {
-    return {
-        description: body.description,
-        full_name: body.full_name,
-        visibility: body.visibility,
-        url: body.url,
-        html_url: body.html_url,
-        created_at: body.created_at,
-        archived: body.archived
-    };
+export const reposMapping = (body: any[]): GitHubRepos[] => {
+    return body.map((obj) => ({
+        description: obj.description,
+        full_name: obj.full_name,
+        visibility: obj.visibility,
+        url: obj.url,
+        html_url: obj.html_url,
+        created_at: obj.created_at,
+        archived: obj.archived
+    }));
 };
 
-export const membersMapping = (body: any): GitHubMembers => {
-    return {
-        login: body.login,
-        url: body.url,
-        html_url: body.html_url,
-        repos_url: body.repos_url
-    };
+export const membersMapping = (body: any[]): GitHubMembers[] => {
+    return body.map((obj) => ({
+        login: obj.login,
+        url: obj.url,
+        html_url: obj.html_url,
+        repos_url: obj.repos_url
+    }));
 };
 
-export const teamsMapping = (body: any): GitHubTeams => {
-    return {
-        name: body.name,
-        description: body.description,
-        url: body.url,
-        html_url: body.html_url,
-        repositories_url: body.repositories_url,
-        members_url: body.members_url
-    };
+export const teamsMapping = (body: any[]): GitHubTeams[] => {
+    return body.map((obj) => ({
+        name: obj.name,
+        description: obj.description,
+        url: obj.url,
+        html_url: obj.html_url,
+        repositories_url: obj.repositories_url,
+        members_url: obj.members_url
+    }));
 };
